18c49d17c58b56f49c9fd41bdad3147c48a640a1
abrook83 <aaronbrook83@gmail.com>
2021-02-18 09:25:45 +0400

    Create Making_Pizzas.py

---
+++ CrashProj/Making_Pizzas.py
@@ -0,0 +1,4 @@
+import pizza as p           # import the module 'pizza', refer in this use as 'p'
+
+p.make_pizza('l','ham','pepperoni','capsicum','cheese','banana')        # call both the module (file) & the function
+p.make_pizza('s','cheese','tears of several small children','artichoke')
