fe1693929b021612dfe9deae672a8f9486181939
abrook83 <aaronbrook83@gmail.com>
2021-02-18 09:27:04 +0400

    Create 5.3-7 Aliens.py

---
+++ CrashProj/5.3-7 Aliens.py
@@ -0,0 +1,46 @@
+# #5.3
+# alien_colour = input("What colour was he? ")
+# if alien_colour == 'green':
+#     print("5 points!")
+
+# #5.4
+# alien_colour = input("What colour was he? ")
+# if alien_colour == 'green':
+#     print("5 points!")
+# else:
+#     print("10 points!")
+
+# #5.5
+# alien_colour = input("What colour was he? ")
+# if alien_colour == 'green':
+#     print("5 points!")
+# elif alien_colour == 'yellow':
+#     print("10 points!")
+# elif alien_colour == 'red':
+#     print("You beauty 15 points!")
+
+# #5.6
+# ask = input("How old are you? ")
+# age = int(ask)
+# if age < 2:
+#     print("You are behbeh")
+# elif age <= 4:
+#     print("You are a brat")
+# elif age <= 13:
+#     print("You definitely a kid")
+# elif age <= 20:
+#     print("Cheer up teenager!")
+# elif age <= 65:
+#     print("You're an adult, so act like it!")
+# elif age > 65:
+#     print("You old motherfucker")
+
+# #5.7
+# faves = ['mango','apple','banana','plum','melon']
+# fruit = input("Name a fruit: ")
+# if fruit in faves:
+#     print("yeah mate, she already in there!")
+# if fruit not in faves:
+#     faves.append(fruit)
+#     print(f"{fruit} added to list")
+# print(faves)
