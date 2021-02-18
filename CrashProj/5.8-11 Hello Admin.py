392f6154acb9a9ed6086036d25df16f09f9c8a1c
abrook83 <aaronbrook83@gmail.com>
2021-02-18 09:27:07 +0400

    Create 5.8-11 Hello Admin.py

---
+++ CrashProj/5.8-11 Hello Admin.py
@@ -0,0 +1,39 @@
+# ### 5.8 & 5.9 Hello Admin ###
+
+# users = ['admin','judy','steve','chris','bill','bob']
+# login = input("Enter username: ")
+# if users:
+#     if login == 'admin':
+#         print(f"Hello {login.title()}, would you like to see a special report?")
+#     elif login in users:
+#         print(f"Hello {login.title()}, welcome back!")
+#     else:
+#         print(f"Sorry {login.title()}, you don't appear to be in the system!")
+# else:
+#     print("We need to find some users!")
+
+
+# ### 5.10 Current Users ###
+
+# curr_users = ['admin','judy','steve','chris','bill','bob']
+# new_users = ['judy','steve','tom','nick','andy']
+# for user in new_users:
+#     if user in curr_users:
+#         print(f"Sorry, the name {user.title()} is already in use\n")
+#     else:
+#         print(f"Welcome {user.title()}, thanks for coming!\n")
+
+
+### 5.11 Ordinal numbers ###
+
+# nums = list(range(1,10))
+# for num in nums:
+#     if num == 1:
+#         print(f"{num}st")
+#     elif num == 2:
+#         print(f"{num}nd")
+#     elif num == 3:
+#         print(f"{num}rd")
+#     else:
+#         print(f"{num}th")
+ 
