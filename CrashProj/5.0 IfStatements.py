27223cddb52c9bea713b5f64050ff0eaeaf5ba12
abrook83 <aaronbrook83@gmail.com>
2021-02-18 09:26:59 +0400

    Create 5.0 IfStatements.py

---
+++ CrashProj/5.0 IfStatements.py
@@ -0,0 +1,49 @@
+# cars = ('bmw','audi','ford','rolls royce','toyota')
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+# req_topping = 'ham'
+# if req_topping != 'anchovies':
+#     print("Hold the anchovies!")
+
+# age0 = 17
+# age1 = 23
+# if age1 >= 18 and age0 <= 23:
+#     print("Go ahead for blue light disco!")
+
+# requested_tops = ['mushrooms','pineapple','ham','salami','mozzarella']
+# req = input("Request a topping: ")
+# if req not in requested_tops:
+#     requested_tops.append(req)
+#     print("Added")
+# else:
+#     print("Sorry, you've already got that!")
+
+# print(requested_tops)
+
+# agin = input("Enter age: ")
+# age = float(agin)
+# if age > 100:
+#     price = 0
+#     print("Get in 'ere old fella")
+# elif age > 80:
+#     price = 10
+# elif age > 21:
+#     price = 25
+# else: price = 15
+# print(f"Your price is ${price}")
+
+# ### PIZZA ####
+# avail_tops = ['cheese','ham','tomato','salami','pineapple','mushroom','beef']
+# req_tops = ['ham','cheese','pineapple','anchovy','beef']
+
+# for requested in req_tops:
+#     if requested in avail_tops:
+#         print(f"Adding {requested}")
+#     else:
+#         print(f"Sorry, we don't have {requested}")
+
+# print("\nWe are making your pie")
