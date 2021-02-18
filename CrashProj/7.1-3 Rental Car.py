380e36ac5318efb1092af03861d4fb8853530569
abrook83 <aaronbrook83@gmail.com>
2021-02-18 09:27:27 +0400

    Create 7.1-3 Rental Car.py

---
+++ CrashProj/7.1-3 Rental Car.py
@@ -0,0 +1,24 @@
+# ### 7.1 Rental Car ###
+
+# want = input("What find of car would you like? ")
+# print(f"Lemme see if I can find you a {want.title()}.")
+
+
+### 7.2 Restaurant Seating ###
+
+# pats = input("For how many people would you like to book? ")
+
+# pats = int(pats)
+# if pats > 8:
+#     print(f"Sorry, {pats} is too many for one table, max. 8 per table!")
+# else: print("Yeah mate, no worries, see you at 7!")
+
+
+### 7.3 Multiples of Ten ###
+
+num = input('Enter a number: ')
+
+num = int(num)
+if num % 10 == 0:
+    print(f"Beauty, {num} is divisible by 10 mate!")
+else: print("Ach, too bad, pick another number.")
