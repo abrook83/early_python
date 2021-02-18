d08b797eeeeaba3486ef0be9319ee36541f334a8
abrook83 <aaronbrook83@gmail.com>
2021-02-18 09:27:40 +0400

    Create 8.1-2 Message.py

---
+++ CrashProj/8.1-2 Message.py
@@ -0,0 +1,16 @@
+### 8.1 Message ###
+
+# def display_message(chapter):
+#     """ Display a message about this chapter """
+#     print(f"This chapter is about {chapter}.")
+
+# display_message('functions')
+
+
+### 8.2 Favourite Book ###
+
+# def fave_book(title):
+#     """ Tells all the user's favourite book """
+#     print(f"One of my favourite books is {title.title()}.")
+
+# fave_book('Shantaram')
