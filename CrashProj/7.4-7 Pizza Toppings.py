15be0b49bced7ab94fbb4c2d8694f0364690a56a
abrook83 <aaronbrook83@gmail.com>
2021-02-18 09:27:30 +0400

    Create 7.4-7 Pizza Toppings.py

---
+++ CrashProj/7.4-7 Pizza Toppings.py
@@ -0,0 +1,39 @@
+# ### 7.4 Pizza Toppings ###
+
+# top = "Enter your topping "
+# top += "\n(Enter 'q' to finish): "
+
+# toppings = []               # empty list of toppings
+
+# while True:
+#     tops = input(top)           # create variable 'tops' from the input
+#     if tops == 'q':
+#         break    
+#     else: 
+#         toppings.append(tops)       # add the input 'tops' to 'list of toppings
+#         continue
+
+# print("You've ordered a pizza with:")
+# for tops in toppings:
+#     print(f"\t{tops.title()}")
+
+
+# ### 7.5 Movie Tickets ###
+
+# age = input("How old are you? ")
+
+# age = int(age)
+
+# active = True
+# while active:
+#     if age <= 3:
+#         print("\nYou go in for free!")
+#         active = False
+#     elif age <= 12:
+#         print("\n$10 for you sonny!")
+#         active = False
+#     else:
+#         print("\nThat'll be $15 old boy!")
+#         active = False
+# print("\n")
+
