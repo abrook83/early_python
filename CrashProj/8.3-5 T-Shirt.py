ba5394bc11197246699f6bd33e663795b0a3b513
abrook83 <aaronbrook83@gmail.com>
2021-02-18 09:27:46 +0400

    Create 8.3-5 T-Shirt.py

---
+++ CrashProj/8.3-5 T-Shirt.py
@@ -0,0 +1,27 @@
+### 8.3 T-Shirt ###
+
+# def make_shirt(size, text):
+#     """ Describe the size and text on the shirt """
+#     print(f"We need a {size} shirt, with '{text}' printed on it!")
+
+# make_shirt('XL', 'Yo Mama!')
+
+
+### 8.4 Large Shirts ###
+
+# def make_shirt(text, size = 'L'):
+#     """ Describe the size and text on the shirt """
+#     print(f"We need a {size} shirt, with '{text}' printed on it!")
+
+# make_shirt('I Love Python','M')
+
+
+### 8.5 Cities ###
+
+# def describe_city(name, country = 'Australia'):
+#     """ Gives the city's name and its country """
+#     print(f"{name.title()} is in {country.title()}.")
+
+# describe_city('Brisbane',)
+
+
