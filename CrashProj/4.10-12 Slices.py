02d83f898d29ef899e959af9be899c6f2dd50e8e
abrook83 <aaronbrook83@gmail.com>
2021-02-18 09:22:19 +0400

    Create 4.10-12 Slices.py

---
+++ CrashProj/4.10-12 Slices.py
@@ -0,0 +1,44 @@
+# #4.10
+# my_foods = ['falafel','pizza','burger','mushrooms','wraps','hotdogs']
+# her_foods = my_foods[:]
+
+# print(f"The first three items in the list are: {my_foods[:3]}")
+# print(f"The middle three items in the list are: {my_foods[2:5]}")
+# print(f"The last three items in the list are: {my_foods[-3:]}")
+
+# #4.11
+# pizzas = ['pepperoni', 'hawaiian', 'godfather', 'meatlovers']
+# friend_pizzas = pizzas[:]
+
+# pizzas.append('cheese')
+# friend_pizzas.append('marisco')
+
+# print("\nMy choice of pizzas are:")
+# for za in pizzas:
+#     print(za.title())
+
+# print("\nMy mate's choice of pizzas are:")
+# for za in friend_pizzas:
+#     print(za.title())
+
+
+
+
+
+#COPIED CODE...
+# for pizza in pizzas:
+#     print(f"\nI love {pizza} pizza!")
+# print("\nYeah mate, it's pizza for me!\n")
+
+
+# for foods in my_foods:
+#     print(f"My fvourite foods include {foods.title()}")
+
+# print("\nChristine's favourites are:")
+# print(her_foods)
+
+# my_foods.append('lasagne')
+# her_foods.append('fish')
+# print("\n")
+# print(my_foods)
+# print(her_foods)
