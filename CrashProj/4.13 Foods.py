3fca5bbc7785ce53dc585f6e110249b33f2c64b1
abrook83 <aaronbrook83@gmail.com>
2021-02-18 09:25:27 +0400

    Create 4.13 Foods.py

---
+++ CrashProj/4.13 Foods.py
@@ -0,0 +1,10 @@
+foods = ('bread','cheese','jam','biscuits','fish')
+print("Menu items:\n")
+for simp in foods:
+    print(simp.title())
+print("\n")
+
+foods = ('bread','cheese','vegemite','cookies','fish')
+print("NEW menu items:\n")
+for simp in foods:
+    print(simp.title())
