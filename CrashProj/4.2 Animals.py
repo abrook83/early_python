675a92c11d24de56961225586a5c03b331358c70
abrook83 <aaronbrook83@gmail.com>
2021-02-18 09:25:34 +0400

    Create 4.2 Animals.py

---
+++ CrashProj/4.2 Animals.py
@@ -0,0 +1,4 @@
+animals = ['kangaroo', 'wombat','quokka','bilby']
+for animal in animals:
+    print(f"A {animal.title()} would make a good pet in 'Straya.\n")
+print("All these 'strayan animals would be good pets, better to leave them in the wild though.")
