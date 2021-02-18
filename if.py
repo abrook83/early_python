0a130db7bd6ee62d2efa7447c1713bbfa444ba94
abrook83 <aaronbrook83@gmail.com>
2021-02-18 09:26:26 +0400

    Create if.py

---
+++ if.py
@@ -0,0 +1,11 @@
+x = input("How much x mate? ")
+xf = int(x)
+if xf > 20:
+    print('yeah mate big one!')
+    if xf < 100:
+        print('not too big though')
+    if xf > 100:
+        print('a bloody whopper!')
+elif xf < 20:
+    print('come back next time')
+print('catch ya')
