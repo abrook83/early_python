e631dbcf8f2b25fc00a461692a83b24a19418d89
abrook83 <aaronbrook83@gmail.com>
2021-02-18 09:26:56 +0400

    Create 4.3-9 CounttoTwenty.py

---
+++ CrashProj/4.3-9 CounttoTwenty.py
@@ -0,0 +1,35 @@
+# #4.3
+# for nums in range(1,21):
+#     print(nums)
+
+# #4.4, 4.5
+# nums = list(range(1,1001))
+# for num in nums:
+#     print(num)
+# print(f"the largest is {max(nums)}")
+# print(f"the smallest is {min(nums)}")
+# print(f"the sum of all is {sum(nums)}")
+
+# #4.6
+# odds = []
+# for num in range(1,21,2):
+#     odds.append(num)
+#     print(num)
+# print(odds)
+
+# #4.7
+# thirds = []
+# for val in range(3,31,3):
+#     print(val)
+
+# #4.8
+# cubes = []
+# for val in range(1,11):
+#     cube = val**3
+#     cubes.append(cube)
+#     print(cube)
+# print(cubes)
+
+# #4.9
+# cubes = [val**3 for val in range(1,11)]
+# print(cubes)
