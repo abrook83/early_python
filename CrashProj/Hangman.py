f4951310ed0f4a268f07f762abbbf84b1fac3c34
abrook83 <aaronbrook83@gmail.com>
2021-02-27 08:17:20 +0400

    early adjust, think how this will work

--- CrashProj/Hangman.py
+++ CrashProj/Hangman.py
@@ -3,16 +3,18 @@ inp = input("Player 1 enter your word: ")
 letters = []
 
 for letter in inp:
-    letters.append(letter)
+    letters.append(letter)      # make a list of the letters in the word
 
-# print(letters)
+w_len = len(letters)
+print(f"Player 2, your word is {w_len} letters long!")
 
-p2_guess = input("Player 2 enter a letter: ")
+
+p2_guess = input("Enter a letter: ")
 
 guesses = []
-guesses.append(p2_guess)
+guesses.append(p2_guess)        # add each guess to the list of guesses
 
 for guess in guesses:
+    guess = guess.lower
     if guess in guesses:
-        print("Got one!")
-        
+        print(f"Yup, {guess} is in the word!")
