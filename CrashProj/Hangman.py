a5fb37104de6254598545a7381e8f321ab736bd2
abrook83 <aaronbrook83@gmail.com>
2021-03-02 14:24:48 +0400

    Able to check inputs against dictionary

--- CrashProj/Hangman.py
+++ CrashProj/Hangman.py
@@ -3,20 +3,32 @@ inp = input("Player 1 enter your word: ")
 inp = inp.lower()
 count = 0
 letters = {}            # empty dictionary
+guesses = {}                        # empty dict of guesses
 
 for letter in inp:
     count += 1
     letters[count] = letter     # add each letter and its place to the dictionary
-    
-# print(letters)
+    guesses[count] = "_"
 
-print(f"\nPlayer 2, your word is {count} letters long...")
+print(f"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer 2, your word is {count} letters long...")
 
-pguess = input("Enter a letter: ")
+for k,v in guesses.items():
+    print(v)            # prints however many blank (under)spaces
 
-guesses = []
-guesses.append(pguess.lower())        # add each guess to the list of guesses
-    # for guess in guesses:
+unsolved = True
+guess_list = []         # empty list of guesses
+
+while unsolved:
+    pguess = input("Enter a letter: ")
+    guess_list.append(pguess.lower())        # add each guess to the dict of guesses
+    for guess in guess_list:
+        if pguess in letters.values():
+            print(f"\n'{pguess}' is correct!")
+            break
+        elif pguess not in letters.values():
+            print("\nSorry, try again.")
+            break
+            
       
 #     guess = guess.lower()
 #     if guess in guesses:
