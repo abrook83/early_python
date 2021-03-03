6e8b3bfc05dcf5a284bb53a48636424f06a21a84
abrook83 <aaronbrook83@gmail.com>
2021-03-03 11:03:26 +0400

    Changed a few titles, visualisations etc

--- CrashProj/Projects/GeneratingData/mpl_squares.py
+++ CrashProj/Projects/GeneratingData/mpl_squares.py
@@ -11,9 +11,9 @@ fig, ax = plt.subplots()
 ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=10)
 
 # set the chart title and label axes
-ax.set_title("Square Numbers", fontsize = 24)
+ax.set_title("Cubed Numbers", fontsize = 24)
 ax.set_xlabel("Value", fontsize = 14)
-ax.set_ylabel("Square of Value", fontsize = 14)
+ax.set_ylabel("Cube of Value", fontsize = 14)
 
 # set the size of tick labels
 ax.tick_params(axis = 'both', labelsize = 14)
