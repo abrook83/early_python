796e3b52ad09a88fede9902cf80ea858e5f478a3
abrook83 <aaronbrook83@gmail.com>
2021-03-04 11:16:19 +0400

    now take data from 2 files for solar vs rainfall

--- CrashProj/Projects/GeneratingData/daly_waters_solar.py
+++ CrashProj/Projects/GeneratingData/daly_waters_solar.py
@@ -11,11 +11,11 @@ with open(filename) as f:
 
     for index, column_header in enumerate(header_row):
         print(index, column_header)
-    
-    # retrieve the dates and high temps from the file
+
+# retrieve the dates and high temps from the first file
     dates, exp_lvls = [], []      # empty list of dates & exposure level
     for row in reader:      # for each row in the opened & read file...
-        current_date = datetime.strptime((f"{row[2]}-{row[4]}-{row[3]}"), '%Y-%d-%m')   # date from row 2, 4, 3
+        current_date = datetime.strptime((f"{row[2]}-{row[4]}-{row[3]}"), '%Y-%d-%m')   # date from row 4, 3
         try:
             exp = float(row[5])  # ...take the 5th column as an integer
         except ValueError:
@@ -24,17 +24,40 @@ with open(filename) as f:
             dates.append(current_date)
             exp_lvls.append(exp)  # add it to the list of highs
 
+print("\n")
+filename1 = 'CrashProj\Projects\data/the_csv_file_format\DalyWatersAirstrip_2020\IDCJAC0009_014626_2020_Data.csv'
+with open(filename1) as f1:
+    reader1 = csv.reader(f1)
+    header_row = next(reader1)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
+      
+    # retrieve the dates and high temps from the second file
+    dates1, rainfalls = [], []      # empty list of dates & exposure level
+    for row in reader1:      # for each row in the opened & read file...
+        current_date1 = datetime.strptime((f"{row[2]}-{row[4]}-{row[3]}"), '%Y-%d-%m')   # date from row 4, 3
+        try:
+            r_fall = float(row[5])  # ...take the 5th column as an integer
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates1.append(current_date1)
+            rainfalls.append(r_fall)  # add it to the list of highs
+
+
     # plot the high & low temps
     plt.style.use('seaborn')
     fig, ax = plt.subplots()
     ax.plot(dates, exp_lvls, c='red', alpha=0.5)     # plot the highs, in red
-    # ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)    # alpha controls a colour's transparency
+    ax.plot(dates1, rainfalls, c='blue', alpha=0.5)     # plot the lows, in blue
+    # ax.fill_between(dates, exp_lvls, rainfalls, facecolor='blue', alpha=0.1)    # alpha controls a colour's transparency
 
     # format the plot
     ax.set_title("Daily Solar Exposure Levels\nDaly Waters, 2020", fontsize=20)
-    ax.set_xlabel('', fontsize=16)
+    ax.set_xlabel("Date", fontsize=16)
     fig.autofmt_xdate()     # draws dates diagonally to prevent overlap
-    ax.set_ylabel("Exposure Level", fontsize=16)
+    ax.set_ylabel("Daily Exposure Level (MJ/m*m)\nAgainst Rainfall (mm)", fontsize=16)
     ax.tick_params(axis='both', which='major', labelsize=16)
 
     plt.show()
