a0cbd7d2e1d39323f255a8c75d767224ea322eba
abrook83 <aaronbrook83@gmail.com>
2021-03-07 11:51:58 +0400

    tidy up

--- CrashProj/Projects/data/mapping_global_data_sets/eq_world_map.py
+++ CrashProj/Projects/data/mapping_global_data_sets/eq_world_map.py
@@ -4,7 +4,7 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # Explore the structure of the data.
-filename = 'data/eq_data_30_day_m1.json'
+filename = 'CrashProj\Projects\data\mapping_global_data_sets\data\eq_data_7_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
@@ -12,14 +12,14 @@ all_eq_dicts = all_eq_data['features']
 
 mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    title = eq_dict['properties']['title']
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
-    hover_texts.append(title)
+    # mag = eq_dict['properties']['mag']
+    # lon = eq_dict['geometry']['coordinates'][0]
+    # lat = eq_dict['geometry']['coordinates'][1]
+    # title = eq_dict['properties']['title']
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_texts.append(eq_dict['properties']['title'])
 
 # Map the earthquakes.
 data = [{
