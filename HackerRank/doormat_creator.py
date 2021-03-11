274feb221d84b4c9a3283221103c686fbc0736bb
abrook83 <aaronbrook83@gmail.com>
2021-03-11 16:43:35 +0400

    remove unrequired

--- HackerRank/doormat_creator.py
+++ HackerRank/doormat_creator.py
@@ -1,5 +1,5 @@
-N, M = map(int, input().split())    # Reads two numbers from input and typecasts them to int using  
-# map function 
+N, M = map(int, input('Enter dimensions: ').split())    # Reads two numbers from input and typecasts them to int using  
+                                    # the map function 
 patt = '.|.'
 for i in range(1,N,2):      # every second number in the range
     patts = i * patt
@@ -16,20 +16,3 @@ for i in reversed(range(1,N,2)):      # every second number in the range
     dashes_per_side = int((M - len(patts)) / 2)
     line = f"{'-' * dashes_per_side}{patts}{'-' * dashes_per_side}"
     print(line)
-# for num in nums:
-#     dashes = 
-
-    # print('-', end='')
-    # for dash in range(0,i):
-    #     print('-', end='')
-
-# nums.reverse()      # reverse the order of nums
-# print(nums)
-
-# N, M = map(int,input().split())
-# for i in range(1,N,2): 
-#     print((i * ".|.").center(M, "-"))
-# # print("WELCOME".center(M,"-"))
-# # for i in range(N-2,-1,-2): 
-# #     print((i * ".|.").center(M, "-"))
-
