40622c3fc9e9b6087622c8583a49bab667598b5e
abrook83 <aaronbrook83@gmail.com>
2021-03-17 14:24:50 +0400

    create rock papaer scissors game

---
+++ 100Days/rock_paper_scissors.py
@@ -0,0 +1,55 @@
+import random
+import rps_visuals
+
+rock = rps_visuals.rock
+paper = rps_visuals.paper
+scissors = rps_visuals.scissors
+
+p1_name = input("Enter player name:\n")
+
+player_win_count = 0
+computer_win_count = 0
+while computer_win_count < 2 or player_win_count < 2:
+  p1_turn = int(input(f'\nYo {p1_name.title()}, enter Rock(1), Paper(2) or Scissors(3):\n'))
+  if p1_turn not in range(1,4):
+    print("Sorry, enter a number between 1 & 3!")
+    continue
+  else:
+    if p1_turn == 1:
+      print(f"\{p1_name.title()} chose: {rock}")
+    elif p1_turn == 2:
+      print(f"\{p1_name.title()} chose: {paper}")
+    else:
+      print(f"\{p1_name.title()} chose: {scissors}")
+    
+    pc_turn = random.randint(1, 3)
+    if pc_turn == 1:
+      print(f"\nComputer chose: {rock}")
+    elif pc_turn == 2:
+      print(f"\nComputer chose: {paper}")
+    else:
+      print(f"\nComputer chose: {scissors}")
+
+    if p1_turn == pc_turn:
+      print("Tied round")
+    elif p1_turn == 1 and pc_turn == 3:
+      print (f"You win this round {p1_name.title()}!")
+      player_win_count +=1
+    elif p1_turn == 3 and pc_turn == 2:
+      print (f"You win this round {p1_name.title()}!")
+      player_win_count +=1
+    elif p1_turn == 2 and pc_turn == 1:
+      print (f"You win this round {p1_name.title()}!")
+      player_win_count +=1
+    else:
+      print("You lose this round!")
+      computer_win_count +=1
+
+  if computer_win_count == 2:
+    print("\nComputer takes the trophy!!" *4)
+    print("\n")
+    break
+  elif player_win_count == 2:
+    print("\nPlayer takes home the gold!!" *4)
+    print("\n")
+    break
