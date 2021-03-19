316f7ba8ffe1820043aa4fca044cade000bea235
abrook83 <aaronbrook83@gmail.com>
2021-03-19 17:55:13 +0400

    traceback, end on wrong guesses not working

--- 100Days/day7_hangman.py
+++ 100Days/day7_hangman.py
@@ -1,3 +1,61 @@
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
 p1_word = input("Player 1 enter your word:\n").lower()
 
 letters = []
@@ -11,17 +69,28 @@ print(f"Player 2 your word is {char_count} letters long!")
 
 for char in p1_word:
     letters.append(char)
-    guess_list.append("_")
+    guess_list += "_"
 
-for char in guess_list:
-    print(char)
+# for char in guess_list:
+#     print(char)
 
-# print(letters)
+print(guess_list)
+wrong_count = 0
 
-while p1_word != guess_list:
+while wrong_count < len(stages):
     p2_guess = input("Enter a letter to guess:\n").lower()
-    for char in letters:
-        if p2_guess == char:
-            guess_list.replace(char)
-        else:
-            print("Woops! Try another one...")
+    if p2_guess not in letters:
+      print(f"\nNope, try another!\n\n{guess_list}")
+      wrong_count +=1
+      print(stages[wrong_count])
+      continue
+    else:
+      for pos in range(len(letters)):
+          letter = p1_word[pos]
+          if letter == p2_guess:
+              guess_list[pos] = letter
+      print(f"\nYup, it's in there!\n\n{guess_list}")
+      if letters == guess_list:
+          break
+
+print("Well done!")
