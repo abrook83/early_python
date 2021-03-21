940d167c3dc010b5e9681ce2372d59e567e223d3
abrook83 <aaronbrook83@gmail.com>
2021-03-21 18:35:46 +0400

    more shortening

--- 100Days/day8_cypher.py
+++ 100Days/day8_cypher.py
@@ -7,29 +7,14 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-def encrypt(plain_text, shift_amt):
+def ceasar(plain_text, shift_amt, cipher_direction):
     result = ''
-    if direction == 'e':
-        for i in plain_text:              # for every letter in the text 
-            pos = alphabet.index(i)
-            new_pos = pos + shift_amt
-            result += alphabet[new_pos]
-        print(f"New text: '{result}' ;)")
-    elif direction == 'd':
-        for i in plain_text:              # for every letter in the text 
-            pos = alphabet.index(i)
-            new_pos = pos - shift_amt
-            result += alphabet[new_pos]
-        print(f"New text: '{result}' ;)")
-    
+    if cipher_direction == 'd':
+        shift_amt *= -1
+    for i in plain_text:              # for every letter in the text 
+        pos = alphabet.index(i)
+        new_pos = pos + shift_amt
+        result += alphabet[new_pos]
+    print(f"New text: '{result}' ;)")
 
-encrypt(plain_text=text, shift_amt=shift)
-
-# def decrypt(encoded_text, shift_amt):
-#     decoded = ''
-#     for i in encoded_text:
-#         pos = alphabet.index(i)
-#         new_pos = pos - shift_amt
-#         decoded += alphabet[new_pos]
-#     print(f"Decoded text: '{decoded}'")
-
+ceasar(plain_text=text, shift_amt=shift, cipher_direction=direction)
