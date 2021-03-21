78694e4138999ec4fe141116300148b3f77515cd
abrook83 <aaronbrook83@gmail.com>
2021-03-21 14:49:23 +0400

    code creation

---
+++ 100Days/day8_cypher.py
@@ -0,0 +1,11 @@
+# Review: 
+# Create a function called greet(). 
+
+def greet_with_name(name, when):
+    print(f"G'day {name}!")
+    print(f"How ya goin' {when} {name}?")
+    print(f"Watcha doin' {when} {name}?")
+
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+greet_with_name(when = 'today', name = "Big Dogg",)
