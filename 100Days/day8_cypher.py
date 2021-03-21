d09b062b094460da07aa870b3ef7d6f18c39f625
abrook83 <aaronbrook83@gmail.com>
2021-03-21 15:19:38 +0400

    prime number checker

--- 100Days/day8_cypher.py
+++ 100Days/day8_cypher.py
@@ -7,13 +7,29 @@ import math
 
 # greet_with_name(when = 'today', name = "Big Dogg",)
 
-## Area Calc
+### Required paint cans calculator ###
 
-test_h = int(input("What is the wall's height?\n"))
-test_w = int(input("What is the wall's width?\n"))
+# test_h = int(input("What is the wall's height?\n"))
+# test_w = int(input("What is the wall's width?\n"))
 
-def calculate_cans(w,h,cover):
-    wall_area = int(w) * int(h)
-    print(f"\nYou'll need {math.ceil(wall_area / cover)} cans of paint.\n")      # math.ciel rounds up!!
+# def calculate_cans(w,h,cover):
+#     wall_area = int(w) * int(h)
+#     print(f"\nYou'll need {math.ceil(wall_area / cover)} cans of paint.\n")      # math.ciel rounds up!!
 
-calculate_cans(test_w,test_h, 5)
+# calculate_cans(test_w,test_h, 5)
+
+### check for prime numbers ###
+
+# num = int(input("Enter a number between 1 & 100:\n"))
+
+# def prime_check(num):
+#     is_prime = True
+#     for i in range(2,num):
+#         if num % i == 0:
+#             is_prime = False
+#     if is_prime:
+#         print(f"{num} is a prime number")
+#     else:
+#         print(f"Nope, {num} is not a prime number")
+
+# prime_check(num)
