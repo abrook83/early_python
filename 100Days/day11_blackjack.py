369f98c56f9f8554e22ebe0cabef659449a256e6
abrook83 <aaronbrook83@gmail.com>
2021-03-24 15:23:27 +0400

    working, getting too ahead of myself

--- 100Days/day11_blackjack.py
+++ 100Days/day11_blackjack.py
@@ -1,2 +1,65 @@
+### House Rules ###
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+
+import random
 from day11_blackjackart import logo, clear
 print(logo)
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+# generate 2 random cards for both player & dealer, show player's cards and score, and dealer's 1st card
+
+player_cards = []
+dealer_cards = []
+
+for card in range(2):
+    player_cards.append(random.choice(cards))
+    player_score = sum(player_cards)
+    if player_score == 21:
+        print("Blackjack, You Win!")
+        game_on = False
+    dealer_cards.append(random.choice(cards))
+    dealer_score = sum(dealer_cards)
+    if dealer_score == 21:
+        print("Dealer Blackjack, You Lose!")
+        game_on = False
+
+print(f"Your cards: {player_cards}")
+print(f"Your score: {player_score}")
+print(f"Dealer's first card: {dealer_cards[0]}")
+# print(dealer_score)
+
+
+while player_score > 21:
+    for card in player_cards:
+        if card != 11:
+            continue
+        else: 
+            card = 1
+
+game_on = True
+
+while game_on:
+    if input("Would you like another card? (y/n):\n") == 'y':
+        player_cards.append(random.choice(cards))
+        player_score = sum(player_cards)
+        print(f"Your cards: {player_cards}")
+        print(f"Your score: {player_score}")
+        if player_score > 21:
+            print("You lose")
+            game_on = False
+        elif player_score == 21:
+            print("Blackjack, You Win!")
+            game_on = False
+        else:
+            continue
+    else:
+        game_on = False
