a55295ec9abb18ac5d0a145a092345e84aaff11a
abrook83 <aaronbrook83@gmail.com>
2021-03-25 15:09:11 +0400

    done, made this bad boy from scratch!

--- 100Days/day12_numberguess.py
+++ 100Days/day12_numberguess.py
@@ -14,29 +14,26 @@ print(logo)
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
 # select the level of difficulty
-level = input("Select difficulty: Easy(e) / Hard(h):\n")
-if level == 'h':
-    guesses = 5
-else:
-    guesses = 10
+guesses = 0
 
-game_on = True
+def select_difficulty():
+    if input("Select difficulty: Easy(e) / Hard(h):\n") == 'h':
+        return guesses + 5
+    else:
+        return guesses + 10
 
 def play(guesses):
-    
-    
-    
-    
-    
+    rand_num = random.randrange(1,100)
+    game_on = True
     while game_on:
-        rand_num = random.randrange(1,100)     
-        while guesses > 0:
+        if guesses == 0:
+            print("You're out of guesses, better luck next time...")
+            game_on = False
+        else: 
             guess = int(input("Guess a number between 1 & 100:\n"))
-            if guesses == 0:
-                game_on = False
             if guess == rand_num:
                 print(f"The answer was {rand_num}, You Win!!")
-                guesses = 0
+                # guesses = 0
                 game_on = False
             elif guess > rand_num:
                 print("Too high")
@@ -48,4 +45,8 @@ def play(guesses):
                 guesses -= 1
                 print(f"You have {guesses} guesses left")
                 continue
+
+if input("Play a game?(y/n):\n") == 'y':
+    guesses = select_difficulty()
+    play(guesses)
 
