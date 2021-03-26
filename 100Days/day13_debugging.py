c498b6ac07c678e82f8f6caf96e585089bea28f6
abrook83 <aaronbrook83@gmail.com>
2021-03-26 16:21:21 +0400

    yikes, that took ages! Forgot the leap year rules

--- 100Days/day13_debugging.py
+++ 100Days/day13_debugging.py
@@ -1,9 +1,36 @@
-#Use a Debugger
-def mutate(a_list):
-  b_list = []
-  for item in a_list:
-    new_item = item * 2
-    b_list.append(new_item)
-  print(b_list)
+## Debug Odd or Even
 
-mutate([1,2,3,5,8,13])
+# # Instructions
+
+# - Read this the code in main.py 
+# - Spot the problems 🐞. 
+# - Modify the code to fix the program. 
+
+# Fix the code so that it works and passes the tests when you submit. 
+
+# # Hint
+
+# 1. Review the previous lesson and go through the 10 steps to tackle these debugging problems.
+
+# # Solution
+
+# [https://repl.it/@appbrewery/day-13-1-solution](https://repl.it/@appbrewery/day-13-1-solution)
+
+# number = int(input("Which number do you want to check?"))
+
+# if number % 2 == 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+year = int(input("Which year do you want to check? "))
+
+if year % 400 == 0:
+    print("Leap year.")
+elif year % 100 == 0:
+    print("Not leap year.")
+elif year % 4 == 0:
+    print("Leap year.")
+else:
+    print("Not leap year.")
+  
