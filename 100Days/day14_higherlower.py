529d80ed1a6a7749691ca9140343fcc001b46397
abrook83 <aaronbrook83@gmail.com>
2021-03-27 11:33:10 +0400

    selects 2 random entries

---
+++ 100Days/day14_higherlower.py
@@ -0,0 +1,28 @@
+import random
+from day14_higherlowerart import logo, clear, vs
+from day14_gamedata import data
+
+# display logo
+print(logo)
+
+# start the game
+# select 2 * random entries from the dictionary
+
+comparison = []
+# takes 2 random entries from the list of data
+for entry in range(2):
+    to_add = random.choice(data)
+    comparison.append(to_add)
+print(comparison)
+
+# display both with vs. logo
+
+# ask for answer from player
+
+# compare answer with actual
+
+# determine winner
+
+# take second entry as first entry to continue play
+
+# ask to play again
