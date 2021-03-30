62011db4604b0ba8e5ef7fc9487648e8fa3cfaa6
abrook83 <aaronbrook83@gmail.com>
2021-03-30 16:48:59 +0400

    final tidy

--- 100Days/day15_coffeemachine.py
+++ 100Days/day15_coffeemachine.py
@@ -6,8 +6,8 @@ from day15_menu import MENU, resources
 def make_coffee(coffee_type, ingredients):
     """Makes the desired coffee from the user input, deducting the resources from the available"""
     print(f"\nMaking 1 * {coffee_type}\n")
-    for item in ingredients:
-        resources[item] -= ingredients[item]
+    for item in ingredients:      # for each item in the imput ingredients list
+        resources[item] -= ingredients[item]       # take the resource amount of the item away from the total resources
 
 
 # TODO generate a report when requested
@@ -15,7 +15,7 @@ def make_coffee(coffee_type, ingredients):
 
 def gen_report():
     """Reports the available resources in the machine"""
-    print("Resources available:")
+    print("Resources available:")           # hold 'alt + shift' to select multipl lines, WHOA!!!
     print(f"Water - {resources['water']}ml")
     print(f"Coffee - {resources['coffee']}g")
     print(f"Milk - {resources['milk']}ml")
@@ -24,13 +24,13 @@ def gen_report():
 # TODO check for sufficient resources to make a drink, feedback to user if not available
 
 
-def check_resources(ingredients):
+def check_resources(ingredients):       # loads in the ingredients list from the MENU item
     """Checks if the requested ingredient is more than is available"""
-    for item in ingredients:
-        if ingredients[item] > resources[item]:
-            print(f"Sorry, we're out of {item}!")
-            return False
-    return True
+    for item in ingredients:            # for each item in that ingredients list...
+        if ingredients[item] > resources[item]:         # if there's more of the ingredient than there is available...
+            print(f"Sorry, we're out of {item}!")       # sorry, we're out
+            return False            # check_resources = False
+    return True         # or else check_resources is True
 
 
 # TODO count coins by the quantity of each coin, calculate the total money input
@@ -49,8 +49,10 @@ def count_coins():
 
 
 def paid(paid_amount, price):
+    """Ensures the paid amount is more than the price, calculates change due"""
     if paid_amount > price:
-        print(f"Change: ${paid_amount - price}")
+        change_due = paid_amount - price
+        print(f"Change: ${change_due:.2f}")     # rounds the change to 2 decimal places
         return True
     else:
         print("Sorry, that's not enough...")
@@ -74,10 +76,10 @@ while machine_avail:
         if check_resources(drink_type["ingredients"]):      # load the ingredients list into the 'check_resources', return a T or F
             to_pay = count_coins()                          # get the total amount paid from 'count_coins'
             if paid(to_pay, drink_type['cost']):            # if paid amount > price...
-                make_coffee(request, drink_type["ingredients"])     # make the coffee
+                make_coffee(request, drink_type["ingredients"])     # make the desired coffee from the input, and its retrieved ingredients
 
 
-#### early workings..... ###
+#### early workings..... #####
 
 
 # if resources['milk'] <= 0:
