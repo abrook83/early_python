cf01774295a2c6386f894f76e9006e7488f86494
abrook83 <aaronbrook83@gmail.com>
2021-03-30 14:40:47 +0400

    getting there, resorted to check the answer....

--- 100Days/day15_coffeemachine.py
+++ 100Days/day15_coffeemachine.py
@@ -3,25 +3,60 @@ from day15_menu import MENU, resources
 # print(resources)
 # TODO 1 "What would you like?" - either coffees, report or OFF function. Afterward, show prompt again to await next direction...
 
-
 def make_coffee(coffee_type):
-    # for coffee_type, ingredient in MENU.items():
+    """Makes the desired coffee from the user input"""
     print(f"\nMaking 1 * {coffee_type}\n")
     water = int(MENU[coffee_type]['ingredients']['water'])
     resources['water'] -= water
-    # print(f"Water req. - {water}ml")
-    # print(f"Water left - {resources['water']}")
     coffee = int(MENU[coffee_type]['ingredients']['coffee'])
     resources['coffee'] -= coffee    
-    # print(f"Coffee req. - {coffee}g")
-    # print(f"Coffee left - {resources['coffee']}")
     if coffee_type == 'espresso':
         pass
     else:
         milk = int(MENU[coffee_type]['ingredients']['milk'])
         resources['milk'] -= milk
-    # print(f"Milk req. - {milk}ml")          # what to do for espresso as it uses no milk
-    # print(f"Milk left - {resources['milk']}")
+
+
+def gen_report():
+    """Reports the available resources in the machine"""
+    print("Resources available:")
+    print(f"Water - {resources['water']}ml")
+    print(f"Coffee - {resources['coffee']}g")
+    print(f"Milk - {resources['milk']}ml")
+
+
+# TODO check for sufficient resources to make a drink, feedback to user if not available
+
+def check_resources(ingredients):
+    """Checks if the requested ingredient is more than is available"""
+    for item in ingredients:
+        if ingredients[item] > resources[item]:
+            print(f"Sorry, we're out of {item}!")
+            return False
+    return True
+
+
+# TODO count coins by the quantity of each coin, calculate the total money input
+
+def count_coins():
+    """Counts the total of the coins input into the machine"""
+    total_in = float(input("How many quarters?: ")) * 0.25
+    total_in += float(input("How many dimes?: ")) * 0.1
+    total_in += float(input("How many nickels?: ")) * 0.05
+    total_in += int(input("How many pennies?: ")) * 0.01
+    return total_in
+
+
+# # TODO calculate change... Determine if transaction successful.
+
+def paid(paid_amount, price):
+    if paid_amount > price:
+        make_coffee()
+        # calculate the change....
+        return True
+    else:
+        print("Sorry, that's no enough...")
+        return False
 
 
 machine_avail = True
@@ -31,17 +66,14 @@ while machine_avail:
     if request == 'off':
         print("Seeya later!")
         machine_avail = False
-    # elif request == 'report':
-    #     gen_report()
+    elif request == 'report':
+        gen_report()
     else:
-        # check_resources()
-        make_coffee(request)
-        print(MENU[request]['ingredients'])
-    
-
-      
-
-
+        drink_type = MENU[request]
+        if check_resources(drink_type["ingredients"]):
+            to_pay = count_coins()
+            if paid(to_pay, drink_type['cost']):
+                make_coffee(request, drink_type["ingredients"])
 
 
 # if resources['milk'] <= 0:
@@ -55,20 +87,12 @@ while machine_avail:
 # while ingredients_avail == True:
 #     make_coffee()
 
-# # TODO check for sufficient resources to make a drink, feedback to user if not available
 
 
 
-# # TODO count coins by the quantity of each coin, calculate the total money input
 
-# def count_coins():
-#     total_in = float(input("How many quarters?: ")) * 0.25
-#     total_in += float(input("How many dimes?: ")) * 0.1
-#     total_in += float(input("How many nickels?: ")) * 0.05
-#     total_in += int(input("How many pennies?: ")) * 0.01
-#     return total_in
 
-# # TODO calculate change... Determine if transaction successful.
+
 
 # # def check_resources(coffee_price, coins_input):          # generate these variables to enter into the function...
 # #     if coins_input > coffee_price:
