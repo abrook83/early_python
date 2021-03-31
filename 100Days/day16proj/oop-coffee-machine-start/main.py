487a6247cb622a0edfe1ece082b30b2144774773
abrook83 <aaronbrook83@gmail.com>
2021-03-31 14:06:27 +0400

    eesh, still not good at classes

--- 100Days/day16proj/oop-coffee-machine-start/main.py
+++ 100Days/day16proj/oop-coffee-machine-start/main.py
@@ -2,25 +2,28 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-money_machine = MoneyMachine
-coffee_maker = CoffeeMaker
-menu = Menu
-menu_item = MenuItem
+# menu = Menu()
+# menu_item = MenuItem()
+
+money_machine = MoneyMachine()      # assigns the class to the variable
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+money_machine.report()              # call the 'report' function from the class
+coffee_maker.report()
 
 machine_avail = True
 
 while machine_avail:
+    options = menu.get_items()
     request = input("What would you like? (espresso, latte or capuccino):\n").lower()
     if request == 'off':
         print("Seeya later!")
         machine_avail = False
     elif request == 'report':
         coffee_maker.report()
-        money_machine.report()
-    else:               
-        menu.find_drink(request)                        # take the coffee type input, take its ingredients from the MENU, label that as 'drink_type'...
-        if CoffeeMaker.is_resource_sufficient(request):      # load the ingredients list into the 'check_resources', return a T or F
-            to_pay = count_coins()                          # get the total amount paid from 'count_coins'
-            if paid(to_pay, drink_type['cost']):            # if paid amount > price...
-                make_coffee(request, drink_type["ingredients"])     # make the desired coffee from the input, and its retrieved ingredients
-
+        money_machine .report()
+    else:
+        drink = menu.find_drink(request)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
