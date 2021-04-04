e4a82c33d1f027a5caa6565daae1cec0f9e89c51
abrook83 <aaronbrook83@gmail.com>
2021-04-04 18:12:08 +0400

    not much today, corry on tomorrow

--- 100Days/day19_moreturtle/race_main.py
+++ 100Days/day19_moreturtle/race_main.py
@@ -1,12 +1,22 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, color
 import turtle
+import random
 
-tim = Turtle()
 screen = Screen()
-screen.setup(width=1000, height=800)     # screen size settings
+screen.setup(width=1000, height=400)     # screen size settings
 
 user_bet = turtle.textinput('Make your bet', 'Which turtle will win the race?')
+colours = ["red", "orange", "yellow", "green", "blue", "purple"]
+y = -((len(colours) / 2) *40)
+
 print(user_bet)
+
+for colour in colours:
+    racer = Turtle(shape="turtle")
+    racer.color(colour)
+    racer.penup()
+    racer.goto(-450,y)
+    y += 40
 
 
 
