0c682395348ece2149d66690b0606594dfa3ef6d
abrook83 <aaronbrook83@gmail.com>
2021-04-06 16:19:52 +0400

    tidy up

--- 100Days/day22_pong/main.py
+++ 100Days/day22_pong/main.py
@@ -14,23 +14,6 @@ screen.tracer(0)        # setting to '0' turns off animation while paddle is cre
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
 
-# paddle = Turtle("square")
-# paddle.shapesize(stretch_wid=5, stretch_len=1)
-# paddle.color("white")
-# paddle.penup()
-# paddle.goto(-350, 0)
-
-
-# def go_up():
-#     new_y = paddle.ycor() + 20
-#     paddle.goto(-350, new_y)
-
-
-# def go_dn():
-#     new_y = paddle.ycor() - 20
-#     paddle.goto(-350, new_y)
-
-
 screen.listen()
 screen.onkey(key="Up", fun=r_paddle.go_up)
 screen.onkey(key="Down", fun=r_paddle.go_dn)
@@ -41,6 +24,5 @@ game_on = True
 
 while game_on:
     screen.update()     # now paddle is created, updates screen to update animation
-
 
 screen.exitonclick()
