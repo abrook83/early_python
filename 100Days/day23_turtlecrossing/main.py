e348eb84d2f792bac73912e193a5c5db1dc143d6
abrook83 <aaronbrook83@gmail.com>
2021-04-08 09:45:48 +0400

    beginning crossy road game

---
+++ 100Days/day23_turtlecrossing/main.py
@@ -0,0 +1,14 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)        # this turns auto screen updating OFF, updating is set in the game's while loop with the timer.
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)     # sets the code's delay, basically this code will run every 0.1 seconds.
+    screen.update()
