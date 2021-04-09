141ed1160e86ad32deb3e542d9c382cd79b4218d
abrook83 <aaronbrook83@gmail.com>
2021-04-09 13:12:20 +0400

    created party invites.....

--- 100Days/day25_mailmerge/Mail Merge Project Start/main.py
+++ 100Days/day25_mailmerge/Mail Merge Project Start/main.py
@@ -2,22 +2,36 @@
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
+### My code ###
 
 # open the names list
-with open("100Days\day25_mailmerge\Mail Merge Project Start\Input\Names\invited_names.txt") as names:
+with open("100Days\day25_mailmerge\Mail Merge Project Start\Input/Names\invited_names.txt") as name_list:
     # for each of the names in the list...
+    names = name_list.readlines()
+    
+with open("100Days\day25_mailmerge\Mail Merge Project Start\Input\Letters\starting_letter.txt") as template:
+    letter = template.read()
     for name in names:
-        # open the starting letter...
-        with open("100Days\day25_mailmerge\Mail Merge Project Start\Input\Letters\starting_letter.txt") as letter:
-            # loop through the words in the template...
-            for word in letter:
-                # if the word is "[name]"...
-                if word == "[name]":
-                    # replace it with the name from the list
-                    word.replace(name)
+        new_name = name.strip()     # can't strip this within the replace method....?
+        new_letter = letter.replace("[name]", new_name)
         # save the new file in the output folder...
+        with open(f"100Days/day25_mailmerge/Mail Merge Project Start/Output/ReadyToSend/letter to {new_name}.txt", mode='w') as letter_to:
+            letter_to.write(new_letter)
+
+
+### Course code ###
+
+# PLACEHOLDER = "[name]"
+
+
+# with open("./Input/Names/invited_names.txt") as names_file:
+#     names = names_file.readlines()
+
+# with open("./Input/Letters/starting_letter.txt") as letter_file:
+#     letter_contents = letter_file.read()
+#     for name in names:
+#         stripped_name = name.strip()
+#         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+#         with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+#             completed_letter.write(new_letter)
