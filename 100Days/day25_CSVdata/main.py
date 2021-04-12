6059a13d5ca002fe5a1e57729a524d4e4c9ff87a
abrook83 <aaronbrook83@gmail.com>
2021-04-12 15:37:05 +0400

    added comments, still getting used to pandas

--- 100Days/day25_CSVdata/main.py
+++ 100Days/day25_CSVdata/main.py
@@ -25,24 +25,34 @@ while game_on:
             if state_answer == state:   # compare the input to names in that column
                 correct_guesses.append(state)   # add it to the dictionary if it is
                 correct_count -= 1              # count down the score
-                
+                """Create a turtle instance to display the correctly
+                guessed state name."""
                 t = turtle.Turtle()
                 t.hideturtle()   
                 t.penup()
+                """Retrieve the row of data for the correct input state's name."""
                 state_data = data[data.state == state_answer]
+                """Take the 'x' and 'y' from that row to input as the 'goto' for the name text."""
                 t.goto(int(state_data.x), int(state_data.y))
-                t.write(state_data.state.item())        # 'item' looks into the data, and takes the first element (state name in this case)
+                t.write(state_data.state.item())        # 'item' looks into the data, and takes the FIRST element (state name in this case)
                                                         # can just code 't.write(state_answer)'
+                """Continue whilst there are still countries left to guess."""
                 if correct_count > 0:                   
                     continue
+                else:
+                    break
 
 for state in data.state:
+    """Compare the list of correct guesses to the list of answers."""
     if not state in correct_guesses:
+        """If an answer is not in the list, add it to the list 'to_learn'."""
         states_to_learn.append(state)
 
 print(f"You missed {correct_count}!")
 print("States to learn:\n")
+"""Create a new variable from the list of states to learn."""
 new_data = pandas.DataFrame(states_to_learn)
+"""Send that data list to a new .csv."""
 new_data.to_csv("100Days\day25_CSVdata/States to learn.csv")
 for state in states_to_learn:
     print(state)
