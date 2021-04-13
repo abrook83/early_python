5bc13b9433d8ab48fa6be105610c8531aee21ef3
abrook83 <aaronbrook83@gmail.com>
2021-04-13 17:38:34 +0400

    lessons on optional arguments

--- 100Days/day27_Tkinter/workings.py
+++ 100Days/day27_Tkinter/workings.py
@@ -1,2 +1,34 @@
 import tkinter
 
+window = tkinter.Tk()
+# set the title -
+window.title("My first GUI program")
+# set the minimum window size -
+window.minsize(800,800)
+
+# create a label -
+label = tkinter.Label(text="I'm a motherfucking label", font=("Arial", 22, "italic"))
+# display the label (call the packer) -
+label.pack(side="left")     # parameters to convey display instructions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""Kept at the end of ALL code, keeps the mainloop running and the window open"""
+window.mainloop()
