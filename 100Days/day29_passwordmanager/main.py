2d67d70d9ca487fe0fe861585397aa8fe6951562
abrook83 <aaronbrook83@gmail.com>
2021-04-19 16:37:19 +0400

    works, added pyperclip!

--- 100Days/day29_passwordmanager/main.py
+++ 100Days/day29_passwordmanager/main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import pyperclip
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -18,7 +20,8 @@ def generate_password():
     pword = "".join(pword_chars)
     # insert the generated password into the password textbox -
     password_entry.insert(0, pword)
-
+    # pyperclip copy's the text onto the clipboard, hit ctrl+v to paste straightaway -
+    pyperclip.copy(pword)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_entry():
