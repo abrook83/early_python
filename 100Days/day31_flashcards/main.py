978a5a73509198afea4ed325c10f4ff7fddea2ea
abrook83 <aaronbrook83@gmail.com>
2021-04-21 17:08:29 +0400

    tired, no more today...

--- 100Days/day31_flashcards/main.py
+++ 100Days/day31_flashcards/main.py
@@ -1,33 +1,45 @@
 from tkinter import *
+import random
+import pandas
 
 BACKGROUND_COLOUR = "#B1DDC6"
+FIRST_WORD = 1
+LAST_WORD = 100
+
+# -------------------------- SELECT WORDS------------------------------ #
+
+data = pandas.read_csv("100Days\day31_flashcards\data\german_words.csv")
+to_learn = data.to_dict(orient="records")       # add to select from range of words....
+
+def next_card():
+    current_card = random.choice(to_learn)
+    card_word = current_card["German"]
+    canvas.itemconfig(title_text, text="German")
+    canvas.itemconfig(word_text, text=card_word)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Flashcards")
 window.config(padx=100, pady=100, bg=BACKGROUND_COLOUR)     # adds padding to the window's borders
 
-front_canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOUR, highlightthickness=0)
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOUR, highlightthickness=0)
 front_image = PhotoImage(file="100Days\day31_flashcards\images\card_front.png")
-front_canvas.create_image(400, 263, image=front_image)
-title_text = front_canvas.create_text(400, 150, text="Title", fill="black", font=("Arial", 36, "italic"))
-word_text = front_canvas.create_text(400, 250, text="Word", fill="black", font=("Arial", 54, "bold"))
-front_canvas.grid(column=1, row=0)
+canvas.create_image(400, 263, image=front_image)
+title_text = canvas.create_text(400, 150, fill="black", font=("Arial", 36, "italic"))
+word_text = canvas.create_text(400, 250, fill="black", font=("Arial", 54, "bold"))
+canvas.grid(column=0, row=0, columnspan=2, padx=5, pady=5)
 
-"""# create the 'Check mark' label"""
-right_canvas = Canvas(width=100, height=100, bg=BACKGROUND_COLOUR, highlightthickness=0)
+"""# create the 'right' mark label"""
 right_image = PhotoImage(file="100Days\day31_flashcards\images/right.png")
-right_canvas.create_image(100, 112, image=right_image)
-right_canvas.grid(column=1, row=1)
+right_button = Button(image=right_image, width=100, height=100, bg=BACKGROUND_COLOUR, command=next_card, highlightthickness=0)
+right_button.grid(column=0, row=1)
 
-# """# create the 'Timer' label"""
-# timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 42, "bold"))
-# timer_label.grid(column=1, row=0)     # parameters to convey display instructions
+"""# create the 'wrong' mark label"""
+wrong_image = PhotoImage(file="100Days\day31_flashcards\images\wrong.png")
+wrong_button = Button(image=wrong_image, width=100, height=100, bg=BACKGROUND_COLOUR, command=next_card, highlightthickness=0)
+wrong_button.grid(column=1, row=1)
 
-# """# create the start button"""
-# start_button = Button(text="start", fg=RED, font=(FONT_NAME, 12, "bold"), command=start_button_click, highlightthickness=0)
-# start_button.grid(column=0, row=2)     # enter grid coordinates for where to place each widget
-
-
+next_card()
 
 window.mainloop()
