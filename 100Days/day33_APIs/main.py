4971e3181edc1739410ad8cfb2710f7c6eb5b782
abrook83 <aaronbrook83@gmail.com>
2021-04-23 12:29:36 +0400

    not feeling it today

--- 100Days/day33_APIs/main.py
+++ 100Days/day33_APIs/main.py
@@ -5,5 +5,11 @@ response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()      # part of the request module, ports HTTP status request responses....
 
 """Retrieve the json data from the source url"""
-data = response.json()["iss_position"]  # data within the "iss_position" heading
-print(data)
+# data = response.json()["iss_position"]  # data within the "iss_position" heading
+# print(data)
+
+long = response.json()["iss_position"]["longitude"]
+lat = response.json()["iss_position"]["latitude"]
+
+position = (long, lat)
+print(position)
