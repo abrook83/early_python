22297fad6a06632dfc948c95657aad9bbc197d65
abrook83 <aaronbrook83@gmail.com>
2021-04-23 12:26:19 +0400

    retrieving data from APIs

--- 100Days/day33_APIs/main.py
+++ 100Days/day33_APIs/main.py
@@ -1,2 +1,9 @@
 import requests
 
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# print(response)     # '200' = success!
+response.raise_for_status()      # part of the request module, ports HTTP status request responses....
+
+"""Retrieve the json data from the source url"""
+data = response.json()["iss_position"]  # data within the "iss_position" heading
+print(data)
