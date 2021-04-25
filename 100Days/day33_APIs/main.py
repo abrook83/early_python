bfaff103e5e5038acfd3109ffac15956502451fb
abrook83 <aaronbrook83@gmail.com>
2021-04-25 14:31:18 +0400

    complete, awaiting email

--- 100Days/day33_APIs/main.py
+++ 100Days/day33_APIs/main.py
@@ -1,26 +1,52 @@
-from tkinter import *
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_EMAIL = "aaronbrook83@gmail.com"
+MY_PASSWORD = "83Tannum19"
+LAT_JLT = 25.074972
+LNG_JLT = 55.144453
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+# print(iss_latitude)
+# print(iss_longitude)
+
+parameters = {
+    "lat": LAT_JLT,
+    "lng": LNG_JLT,
+    "formatted": 0,
+}
+
+def station_overhead():
+    if LAT_JLT+5 <= iss_latitude <= LAT_JLT-5 and LNG_JLT+5 <= iss_longitude <= LNG_JLT-5:
+        return True
 
 
-def get_quote():
-    pass
-    #Write your code here.
+def night_time():
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now().hour
+    if time_now >= sunset or time_now <= sunrise:
+        return True
 
-
-
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
-
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
-canvas.grid(row=0, column=0)
-
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
-
-
-
-window.mainloop()
+while True:
+    time.sleep(60)
+    if night_time() and station_overhead():
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg="Subject: Look Up! \n\nThe ISS is above you in the sky!"
+        )
--- 100Days/day33_APIs/workings.py
+++ 100Days/day33_APIs/workings.py
@@ -1,15 +1,37 @@
 import requests
+from datetime import date, datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# print(response)     # '200' = success!
-response.raise_for_status()      # part of the request module, ports HTTP status request responses....
+LAT_JLT = 25.074972
+LNG_JLT = 55.144453
 
-"""Retrieve the json data from the source url"""
-# data = response.json()["iss_position"]  # data within the "iss_position" heading
-# print(data)
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# # print(response)     # '200' = success!
+# response.raise_for_status()      # part of the request module, ports HTTP status request responses....
 
-long = response.json()["iss_position"]["longitude"]
-lat = response.json()["iss_position"]["latitude"]
+# """Retrieve the json data from the source url"""
+# # data = response.json()["iss_position"]  # data within the "iss_position" heading
+# # print(data)
 
-position = (long, lat)
-print(position)
+# long = response.json()["iss_position"]["longitude"]
+# lat = response.json()["iss_position"]["latitude"]
+
+# position = (long, lat)
+# print(position)
+
+parameters = {
+    "lat": LAT_JLT,
+    "lng": LNG_JLT,
+    "formatted": 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+timenow = datetime.now()
+
+print(sunrise)
+print(sunset)
+print(timenow.hour)
