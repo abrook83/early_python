9f9ee7d298b6f81c8efc6f8826160392a9c1a663
abrook83 <aaronbrook83@gmail.com>
2021-04-28 16:10:28 +0400

    set up with twilio to send sms'

--- 100Days/day35_APIkeys/workings.py
+++ 100Days/day35_APIkeys/workings.py
@@ -1,11 +1,16 @@
 import requests
+from twilio.rest import Client
+
+account_sid = "ACb66e2921da54242801402ad841f3b03e"
+auth_token = "ca86aa21f9b1aabce0902e1d813bc26e"
+
 # from datetime import date, datetime
 
 endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 
 weather_params = {
-    'lat': 52.085339,
-    'lon': 8.742500,
+    'lat': 45.777210,
+    'lon': 3.082520,
     'exclude': 'current,minutely,daily',
     'appid': "293d14183fd04a680f4133160d9cc149",
 }
@@ -22,4 +27,13 @@ for _ in range(12):
         will_rain = True
 
 if will_rain:
-    print("Don't forget your umbrella!")
+    # print("rain coming")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="Looks like rain, better take your umbrella!",
+        from_="+18326484615",
+        to="+971501054140"
+    )
+
+print(message.status)
