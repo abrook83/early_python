658c436eba0fc89ba50c15e59ea10adb396c8cb6
abrook83 <aaronbrook83@gmail.com>
2021-04-29 15:51:23 +0400

    tidying up

--- 100Days/day36_stockAPIs/stock-news-hard-start/main.py
+++ 100Days/day36_stockAPIs/stock-news-hard-start/main.py
@@ -24,27 +24,25 @@ news_params = {
     "apiKey": "0d60b23561254d3fb2a2a25a8ff6ba7f",
 }
 
-## STEP 1: Use https://newsapi.org/docs/endpoints/everything
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-#HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. 
-# e.g. 40 - 20 = -20, but the positive difference is 20.
-#HINT 2: Work out the value of 5% of yerstday's closing stock price. 
-
 response = requests.get(STOCK_ENDPOINT, params=stock_params)
 response.raise_for_status()
 data = response.json()
+
+"""Plug the dates in to retrieve the final stock prices"""
 yesterday_end_price = float(data["Time Series (Daily)"][str(yesterday)]["4. close"])
 print(f"Yesterday's closing price: {yesterday_end_price}")
 day_before_end_price = float(data["Time Series (Daily)"][str(day_before)]["4. close"])
 print(f"The day before's closing price: {day_before_end_price}")
 
+"""Calculate the price fluctuation"""
 price_change = yesterday_end_price - day_before_end_price
 price_change = abs(price_change)    # abs changes the number to an absolute value (no '-')
 print(f"Difference: {round(price_change, 3)}")
 percent_change = (price_change / yesterday_end_price) * 100
-print(f"Percentage change: {round(percent_change, 3)}%")      # format the number down to fewer decimal points...
+print(f"Percentage change: {round(percent_change, 3)}%")      # format to 3 decimal points
+
+"""Retrieve news headlines if fluctuations are > 5%"""
 if percent_change >= 1:
-    # print("Get news!")
     news_response = requests.get(NEWS_ENDPOINT, params=news_params)
     news = news_response.json()
     for _ in range(3):
@@ -54,28 +52,3 @@ if percent_change >= 1:
         print(f"\nDesciption: {desc}\n")
 else:
     print("Minor fluctuations, don't be greedy...")
-
-
-## STEP 2: Use https://newsapi.org/docs/endpoints/everything
-# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
-#HINT 1: Think about using the Python Slice Operator
-
-
-
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number. 
-#HINT 1: Consider using a List Comprehension.
-
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
