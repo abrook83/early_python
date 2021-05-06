5733b6e78a839de42316ebbf312621e7e56d960e
abrook83 <aaronbrook83@gmail.com>
2021-05-06 14:35:26 +0200

    creating colour graph

--- 100Days/day37_habittracking/workings.py
+++ 100Days/day37_habittracking/workings.py
@@ -1,13 +1,34 @@
 import requests
 
+UNAME = "abrook"
+TOKEN = "chooka37"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
-params = {
-    'token': 'chooka37',
-    'username': 'abrook',
-    'agreeTermsOfService': 'yes',
-    'notMinor': 'yes',
+user_params = {
+    "token": TOKEN,
+    "username": UNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
 }
 
-# response = requests.post(url=pixela_endpoint, json=params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{UNAME}/graphs"
+
+graph_params = {
+    "id": "graph1",
+    "name": "Coding & Study Graph",
+    "unit": "Hours",
+    "type": "float",
+    "color": "sora",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+
+response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+print(response.text)
