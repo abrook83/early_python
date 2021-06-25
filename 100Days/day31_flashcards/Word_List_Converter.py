4124e367acece529a19b8086be61e4e4b1a4adbc
abrook83 <aaronbrook83@gmail.com>
2021-06-25 16:54:17 +0200

    Parsed into csv, need to allow for DE characters

--- 100Days/day31_flashcards/Word_List_Converter.py
+++ 100Days/day31_flashcards/Word_List_Converter.py
@@ -1,18 +1,20 @@
+import csv
 # Access and open the file -
 with open(r"100Days\day31_flashcards/Nouns Frequency List.txt") as de_words_4000:
     lines = de_words_4000.readlines()
 
 # decode the german letters
 
+csv_file = open("DE_Words.csv", "w")
+writer = csv.writer(csv_file)
+
 # turn each line into a list split at the '-' and '~'
-for line in lines[:5]:
+for line in lines:
     words = line.split('-')
-    en_word = words[0].split()[1]
-    de_word = words[1].split('~')[0]
-    de_plural = words[1].split('~')[1]
-    print(f"Eng Word: {en_word}")
-    print(f"DE Word: {de_word}")
-    print(f"DE Plural: {de_plural}")
+    en_word = words[0].split()[1].strip()
+    de_word = words[1].split('~')[0].strip()
+    de_plural = words[1].split('~')[1].strip()
+    new_string = f"{en_word},{de_word},{de_plural}\n"
+    csv_file.write(new_string)
 
-
-
+csv_file.close()
