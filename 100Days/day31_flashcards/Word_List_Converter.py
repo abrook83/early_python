d6f9213b925a587a263562cc29e9a91946024ddd
abrook83 <aaronbrook83@gmail.com>
2021-06-25 16:40:00 +0200

    lines split into en, de & plural

--- 100Days/day31_flashcards/Word_List_Converter.py
+++ 100Days/day31_flashcards/Word_List_Converter.py
@@ -1,18 +1,18 @@
 # Access and open the file -
-with open(r"100Days\day31_flashcards\Nouns Frequency List.txt") as de_words_4000:
+with open(r"100Days\day31_flashcards/Nouns Frequency List.txt") as de_words_4000:
     lines = de_words_4000.readlines()
 
 # decode the german letters
 
 # turn each line into a list split at the '-' and '~'
 for line in lines[:5]:
-    words = line.split()
-    en_word = words[1]
-    de_words = words[3:5]
-    de_plural = words[6:]
-    print(en_word)
-    print(de_words)
-    print(de_plural)
+    words = line.split('-')
+    en_word = words[0].split()[1]
+    de_word = words[1].split('~')[0]
+    de_plural = words[1].split('~')[1]
+    print(f"Eng Word: {en_word}")
+    print(f"DE Word: {de_word}")
+    print(f"DE Plural: {de_plural}")
 
 
 
