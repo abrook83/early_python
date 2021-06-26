eef44f882db913d5dc0017e89a4d437cb60fdcae
abrook83 <aaronbrook83@gmail.com>
2021-06-26 10:37:25 +0200

    Not sure I can save a csv with umlauts

--- 100Days/day31_flashcards/Word_List_Converter.py
+++ 100Days/day31_flashcards/Word_List_Converter.py
@@ -1,7 +1,8 @@
 import csv
 # Access and open the file -
-with open(r"100Days\day31_flashcards/Nouns Frequency List.txt") as de_words_4000:
-    lines = de_words_4000.readlines()
+DE_dict = open("100Days\day31_flashcards/Nouns Frequency List.txt", encoding='utf-8', errors='ignore')
+lines = DE_dict.readlines()
+print(lines[:5])
 
 # decode the german letters
 
@@ -9,11 +10,12 @@ csv_file = open("DE_Words.csv", "w")
 writer = csv.writer(csv_file)
 
 # turn each line into a list split at the '-' and '~'
-for line in lines:
+for line in lines[:8]:
+    print(line)
     words = line.split('-')
-    en_word = words[0].split()[1].strip()
-    de_word = words[1].split('~')[0].strip()
-    de_plural = words[1].split('~')[1].strip()
+    en_word = words[0].split()[1].strip().encode('utf-8','ignore')
+    de_word = words[1].split('~')[0].strip().encode('utf-8','ignore')
+    de_plural = words[1].split('~')[1].strip().encode('utf-8','ignore')
     new_string = f"{en_word},{de_word},{de_plural}\n"
     csv_file.write(new_string)
 
